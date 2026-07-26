0e3a7939f116dedb00fe110e245855a881442fbb
Juluie <124674334+Juluie@users.noreply.github.com>
2026-07-26 16:49:40 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('AM-0 init');
+
+  if (typeof setMode !== 'function') {
+    console.error('Ошибка: setMode не найден. Проверь порядок подключения скриптов.');
+    return;
+  }
+
+  setMode('photo');
+});
 
