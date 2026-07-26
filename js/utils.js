7e0713dc9867926bc174e749990b46b9b6accfcb
Juluie <124674334+Juluie@users.noreply.github.com>
2026-07-26 16:55:10 +0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,6 @@
-function gaussianBlur(gray, w, h, radius) {
+<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.0/p5.min.js"></script>
+<script>
+  function gaussianBlur(gray, w, h, radius) {
   if (radius <= 0) return gray;
   const sigma = Math.max(0.1, radius / 2);
   const size = radius * 2 + 1;
